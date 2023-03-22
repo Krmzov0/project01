28e2a49ce34006070ccda7834ddf8e69f2aefa01
Krmzov0 <krmzovgorgi0@gmail.com>
2023-03-22 21:32:06 +0100

    UI Changes

--- src/Components/MainPage/index.jsx
+++ src/Components/MainPage/index.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { UserAuth } from '../../Context/authContext'
 import Header from '../Header/';
 import { Link } from 'react-router-dom'
-import gif from '../../Assets/Images/urban-842.gif'
 
 function MainPage() {
   const { user } = UserAuth();
@@ -34,11 +33,6 @@ function MainPage() {
 
             <Link to='/create-script' className='fixed bottom-6 sm:bottom-auto sm:relative z-40'><button className='mt-4 relative w-max px-[2px] py-[2px] pr-8 rounded-full bg-[#FDCA40]  flex justify-center items-center text-lg gap-x-4 font-medium'><div className='p-4 bg-[#1c1c1c] rounded-full'><Add size="26" color="#fff" /></div> Create Script </button></Link>
           </div>
-        </div>
-
-
-        <div className='flex place-self-end w-full h-max py-8 bg-[#fff]'>
-          <img className='z-50' src={gif} alt="" />
         </div>
       </div>
     </>
